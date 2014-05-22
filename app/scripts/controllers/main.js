e9581de2fd675ab88f01feccd63126e94a6a5fd8
alainncls <alain.nicolas.perso@gmail.com>
2014-05-22 09:05:25 +0200

    Début de journée

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,4 +25,16 @@ angular.module('appQuoteApp')
 			$scope.quotes.splice($index, 1);
 		});
 	}
+	$scope.removeNew = function($id, $index) {
+		$http.delete('http://192.168.14.138:9292/quotes/news/'+$id).success(function(data){
+			$scope.news.splice($index, 1);
+		});
+	}
+	$scope.addNew = function() {
+		$scope.news.push({news:$scope.shortN, author:$scope.author, });
+
+		$http.post('http://192.168.14.138:9292/quotes/news', {news_short:$scope.shortN, author:$scope.authorN, news_long:'', date:new Date()}).success(function(data){});
+		$scope.shortN = '';
+		$scope.authorN = '';
+	};
   });
